87bb2062429494b84472f9d57c17d879a009cd4a
manu <facos86@gmail.com>
2024-06-07 11:28:12 +0200

    Updated logger configuration for MongoDB
    The configuration for the 'pino-mongodb' target in the logger has been modified to include the database host. The URI has been changed from using 'mongo' as the hard-coded host name to using the 'host' value from the database configuration.

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -2,20 +2,22 @@ import pino from 'pino';
 import { db } from './config';
 
 const transport = pino.transport({
-  targets: [{
-    options: {
-      colorize: true
-    },
-    target: 'pino-pretty' // must be installed separately
-  },
+  targets: [
     {
-    target: 'pino-mongodb',
       options: {
-        uri: `mongodb://mongo:${db.port}/`,
+        colorize: true,
+      },
+      target: 'pino-pretty', // must be installed separately
+    },
+    {
+      target: 'pino-mongodb',
+      options: {
+        uri: `mongodb://${db.host}:${db.port}/`,
         database: `${db.name}`,
         collection: 'logs',
-      }
-  }]
+      },
+    },
+  ],
 });
 
 export default pino(transport);
