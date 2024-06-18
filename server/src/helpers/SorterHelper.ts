a923255a528cb43cae9b4e9565298c1e390daed0
manu <facos86@gmail.com>
2024-06-18 09:58:03 +0200

    Add tests and enhancements for helper functions
    This update includes comprehensive tests for `PaginationHelper`, `SorterHelper`, and `FilterHelper`. Additionally, error handling has been added to `SorterHelper` to deal with invalid JSON, and `PaginationHelper` has been tweaked to return an empty array if passed null or undefined data.

--- server/src/helpers/SorterHelper.ts
+++ server/src/helpers/SorterHelper.ts
@@ -1,23 +1,34 @@
+import logger from '../logger';
+
 export function sortByFields<T>(data: T[], params: any) {
-  if (params.sorter) {
-    const sorter = JSON.parse(params.sorter);
+  let sorter: Record<keyof T, string> | null = null;
+  try {
+    sorter = params.sorter ? JSON.parse(params.sorter) : null;
+  } catch (error) {
+    logger.error('Could not parse sorter parameter');
+    return data;
+  }
+
+  if (sorter) {
     return data.sort((prev, next) => {
       let sortNumber = 0;
       (Object.keys(sorter) as Array<keyof T>).forEach((key) => {
-        const nextSort = next[key] as string;
-        const preSort = prev[key] as string;
-        if (sorter[key] === 'descend') {
-          if (preSort.localeCompare(nextSort) > 0) {
-            sortNumber += -1;
-          } else {
-            sortNumber += 1;
+        if (typeof prev[key] === 'string' && typeof next[key] === 'string') {
+          const nextSort = next[key] as string;
+          const preSort = prev[key] as string;
+          if (sorter[key] === 'descend') {
+            if (preSort.localeCompare(nextSort) > 0) {
+              sortNumber += -1;
+            } else {
+              sortNumber += 1;
+            }
+            return;
           }
-          return;
-        }
-        if (preSort.localeCompare(nextSort) > 0) {
-          sortNumber += 1;
-        } else {
-          sortNumber += -1;
+          if (preSort.localeCompare(nextSort) > 0) {
+            sortNumber += 1;
+          } else {
+            sortNumber += -1;
+          }
         }
       });
       return sortNumber;
