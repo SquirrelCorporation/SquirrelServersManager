bcfdbe90074354c789291a7e1a0e86bb967a1235
Squirrel Developer <squirrel@squirrelserversmanager.io>
2024-08-29 12:54:48 +0200

    Add Smart icon component and replace inline SVG usage
    Introduced a new Smart icon component in CustomIcons.tsx and replaced its inline SVG usage in PlaybookExecutionTerminalModal.tsx with the new component. This improves code readability and consistency in the icon management across the application.

--- client/src/components/Icons/CustomIcons.tsx
+++ client/src/components/Icons/CustomIcons.tsx
@@ -1395,3 +1395,22 @@ const ConfigurationSolidSvg = (props: any) => (
 export const ConfigurationSolid = (
   props: Partial<CustomIconComponentProps>,
 ) => <Icon component={ConfigurationSolidSvg} {...props} />;
+
+const SmartSvg = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M256.3 19.95c-41 0-74.1 32.7-74.1 73.27c0 22.98 8 33.78 16.8 47.78c5.9 9.3 12 20.1 15.5 35.6h83c3.5-15.9 9.6-26.8 15.5-36.2c8.9-14.1 16.8-24.7 16.8-47.18c0-40.57-32.8-73.27-73.5-73.27zm-142.1 7.7L81 35.4l81.2 40.25l-48-48zm283.6 0l-48 48L431 35.4l-33.2-7.75zM210.5 79.2l45.5 22.7l45.5-22.7l-20.8 83l-17.4-4.4l11.2-45l-18.5 9.3l-18.5-9.3l11.2 45l-17.4 4.4l-20.8-83zM64 96.03v32.07l96-16.1l-96-15.97zm384 0L352 112l96 16V96.03zM334.2 144.3l39.9 63.3l24.1-15.3l-64-48zm-156.4.1l-64 48l24.1 15.3l39.9-63.3zM216 191v16h80v-16h-80zm34.4 28.3c-13.7 0-26.9.5-35.7 1c-68.2 10.7-82.9 105.4-66.7 191.6h23.6l-1-105.4l18.6-.2c-1.4 63.7 1.6 126.6 5.5 189.7h51.4V390.3h18.7V496h50.4c4.5-65 5.9-131.5 6.5-189.7l18.7.2l-1.1 105.4h24.6c18.3-88.5-4.8-178.9-67.1-190.6c-9.4-1.4-24.9-2-40.6-2h-5.8z"
+    />
+  </svg>
+);
+
+export const Smart = (props: Partial<CustomIconComponentProps>) => (
+  <Icon component={SmartSvg} {...props} />
+);
