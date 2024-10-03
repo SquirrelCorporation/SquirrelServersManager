1ddbff10af2c357ad8b1cf06dda4b1dabd1938de
Squirrel Developer <squirrel@squirrelserversmanager.io>
2024-10-03 10:32:34 +0200

    Simplify error handling in Validator middleware.
    Refactor error handling by extracting error messages earlier and simplifying the JSON response. This improves readability and maintains the same functionality.

--- server/src/middlewares/Validator.ts
+++ server/src/middlewares/Validator.ts
@@ -1,14 +1,12 @@
 import { validationResult } from 'express-validator';
-import { NextFunction, Request, Response } from 'express';
 import logger from '../logger';
 
-const validator = (req: Request, res: Response, next: NextFunction) => {
+const validator = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    logger.error(errors.array());
-    return res
-      .status(400)
-      .json({ success: false, message: JSON.stringify(errors.array().map((e) => e.msg)) });
+    const errorMessages = errors.array().map((e) => e.msg);
+    logger.error(errorMessages);
+    return res.status(400).json({ success: false, message: errorMessages });
   }
   next();
 };
