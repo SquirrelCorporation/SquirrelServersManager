c3353c0c4e6faf2a2d2a40fd7d154d7ea3ea663b
SquirrelDevelopper <squirrel@squirrelserversmanager.io>
2024-10-10 10:20:10 +0200

    Try to fix runtime prod error (express)

--- client/production-server.js
+++ client/production-server.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static('dist'));
-app.get('*', function (req, res) {
+app.get(/.*/, function (req, res) {
   return res.sendFile(__dirname + '/dist/index.html');
 });
 
