d8d3ae35ae9c246621a581c2759dc22bb0716553
SquirrelDevelopper <squirrel@squirrelserversmanager.io>
2024-11-07 14:39:14 +0100

    Add chmod command for temporary private key files
    Ensure that temporary private key files are securely permissioned by applying chmod 600. This enhances the security by restricting file access to the owner only.

--- server/src/modules/shell/managers/SshPrivateKeyFileManager.ts
+++ server/src/modules/shell/managers/SshPrivateKeyFileManager.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import logger from '../../../logger';
 import { DEFAULT_VAULT_ID, vaultDecrypt } from '../../ansible-vault/ansible-vault';
 import { AbstractShellCommander } from '../AbstractShellCommander';
+import shellWrapper from '../ShellWrapper';
 import FileSystemManager from './FileSystemManager';
 
 class SshPrivateKeyFileManager extends AbstractShellCommander {
@@ -22,6 +23,9 @@ class SshPrivateKeyFileManager extends AbstractShellCommander {
     const decryptedContent = await vaultDecrypt(sskVaultedKey, DEFAULT_VAULT_ID);
     const tmpKeyFilePath = this.getTmpKeyFilePath(this.getTmpKeyFileName(execUuid, deviceUuid));
     FileSystemManager.writeFile(decryptedContent as string, tmpKeyFilePath);
+    if (this.executeCommand(shellWrapper.chmod, '600', tmpKeyFilePath).code !== 0) {
+      throw new Error(`genAnsibleTemporaryPrivateKey - Error chmoding file ${tmpKeyFilePath}`);
+    }
     return tmpKeyFilePath;
   }
 
