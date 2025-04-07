e0c6e7a578b26242c8aabdcd67e2f9ded7e28097
SquirrelDeveloper <facos86+squirrel@gmail.com>
2025-04-07 10:40:18 +0200

    Document remaining broken features

---
+++ server/TO_FIX.md
@@ -0,0 +1,2 @@
+Ansible Vault (CRUD)
+Repositories (CRUD)
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,31 +1,40 @@
+/*
+ Configuration
+*/
+// TODO: use embeded NestJS
 export const db = {
-  name: process.env.DB_NAME || 'ssm',
-  host: process.env.DB_HOST || 'mongo',
-  port: process.env.DB_PORT || '27017',
-  user: process.env.DB_USER || '',
-  password: process.env.DB_USER_PWD || '',
-  minPoolSize: parseInt(process.env.DB_MIN_POOL_SIZE || '5'),
-  maxPoolSize: parseInt(process.env.DB_MAX_POOL_SIZE || '10'),
+  name: process.env.DB_NAME || "ssm",
+  host: process.env.DB_HOST || "mongo",
+  port: process.env.DB_PORT || "27017",
+  user: process.env.DB_USER || "",
+  password: process.env.DB_USER_PWD || "",
+  minPoolSize: parseInt(process.env.DB_MIN_POOL_SIZE || "5"),
+  maxPoolSize: parseInt(process.env.DB_MAX_POOL_SIZE || "10"),
 };
 
 export const redisConf = {
-  host: process.env.REDIS_HOST || '',
-  port: parseInt(process.env.REDIS_PORT || '6379'),
+  host: process.env.REDIS_HOST || "",
+  port: parseInt(process.env.REDIS_PORT || "6379"),
 };
 
 export const prometheusConf = {
   host: process.env.PROMETHEUS_HOST || `http://prometheus:9090`,
   baseURL: process.env.PROMETHEUS_BASE_URL || `/api/v1`,
-  user: process.env.PROMETHEUS_USERNAME || 'user',
-  password: process.env.PROMETHEUS_PASSWORD || 'pass',
+  user: process.env.PROMETHEUS_USERNAME || "user",
+  password: process.env.PROMETHEUS_PASSWORD || "pass",
 };
 
 // Ensure SECRET has a default value for development environments
 export const SECRET =
   process.env.SECRET ||
-  (process.env.NODE_ENV === 'development' ? 'WLZBQ9UozypQJ8p8LLHIMZ0ZuSyY6uTY' : '');
-export const VAULT_PWD = process.env.VAULT_PWD || '';
-export const SESSION_DURATION = parseInt(process.env.SESSION_DURATION || '86400000');
-export const SSM_INSTALL_PATH = process.env.SSM_INSTALL_PATH || '/opt/squirrelserversmanager';
-export const SSM_DATA_PATH = process.env.SSM_DATA_PATH || '/data';
-export const TELEMETRY_ENABLED = process.env.TELEMETRY_ENABLED === 'true';
+  (process.env.NODE_ENV === "development"
+    ? "WLZBQ9UozypQJ8p8LLHIMZ0ZuSyY6uTY"
+    : "");
+export const VAULT_PWD = process.env.VAULT_PWD || "";
+export const SESSION_DURATION = parseInt(
+  process.env.SESSION_DURATION || "86400000"
+);
+export const SSM_INSTALL_PATH =
+  process.env.SSM_INSTALL_PATH || "/opt/squirrelserversmanager";
+export const SSM_DATA_PATH = process.env.SSM_DATA_PATH || "/data";
+export const TELEMETRY_ENABLED = process.env.TELEMETRY_ENABLED === "true";
