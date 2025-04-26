a056066a759602f6424bd388879e45c5625ba32a
Squirrel Dev <squirrel@squirrelserversmanager.io>
2025-04-26 22:14:08 +0200

    Remove tsconfig-paths plugin from Vitest configuration

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,9 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     environment: 'node',
